0e2361045ba9faf3a7d74c0e476646e5e6709b04
tin <tinsatthu1988@gmail.com>
2021-07-11 19:22:09 +0700

    004 Code Role Entity, Role Repository and Unit Test

--- Shopme/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/ShopmeBackEndApplication.java
+++ Shopme/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/ShopmeBackEndApplication.java
@@ -1,9 +1,12 @@
-package com.shopme;
+package com.shopme.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EntityScan({"com.shopme.common.entity","com.shopme.admin.user"})
 public class ShopmeBackEndApplication {
 
     public static void main(String[] args) {
---
+++ Shopme/ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/RoleRepositoryTests.java
@@ -0,0 +1,40 @@
+package com.shopme.admin.user;
+
+import com.shopme.common.entity.Role;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class RoleRepositoryTests {
+
+    @Autowired private RoleRepository repo;
+
+    @Test
+    public void testCreateFirstRole(){
+        Role roleAdmin = new Role("Admin","manage everything");
+        Role savedRole = repo.save(roleAdmin);
+    }
+
+    @Test
+    public void testCreatRestRoles() {
+        Role roleSalesperson = new Role("Salesperson","Manage product price, customers, shipping, orders and sales report");
+
+        Role roleEditor = new Role("Editor","Manage categories, products, brands, articles and menus");
+
+        Role roleShipper = new Role("Shipper","View products, view orders and update order status");
+
+        Role roleAssistant = new Role("Assistant","Manage questions and reviews");
+
+        repo.saveAll(List.of(roleSalesperson,roleEditor,roleShipper, roleAssistant));
+    }
+}
