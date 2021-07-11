98b4d38c55f8904c4b6e80da313bd6d9e1dd5b85
tin <tinsatthu1988@gmail.com>
2021-07-11 13:16:52 +0700

    test 3

--- Shopme/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/ShopmeFrontEndApplication.java
+++ Shopme/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/ShopmeFrontEndApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShopmeBackEndApplication {
+public class ShopmeFrontEndApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ShopmeBackEndApplication.class, args);
+        SpringApplication.run(ShopmeFrontEndApplication.class, args);
     }
 
 }
