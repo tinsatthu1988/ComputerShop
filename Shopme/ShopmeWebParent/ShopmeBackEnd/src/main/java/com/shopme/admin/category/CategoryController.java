ce798fee88902b93e17f39cfbeae4ba97729221f
tin <tinsatthu1988@gmail.com>
2021-07-18 22:25:35 +0700

    015 Code Filter Function for Categories List

--- Shopme/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryController.java
+++ Shopme/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryController.java
@@ -25,25 +25,37 @@ public class CategoryController {
 
     @GetMapping("/categories")
     public String listFirstPage(@Param("sortDir") String sortDir, Model model) {
-        return listByPage(1, sortDir, model);
+        return listByPage(1, sortDir,null, model);
     }
 
     @GetMapping("/categories/page/{pageNum}")
     public String listByPage(
-            @PathVariable(name="pageNum") int pageNum, @Param("sortDir") String sortDir, Model model) {
+            @PathVariable(name="pageNum") int pageNum,
+            @Param("sortDir") String sortDir,
+            @Param("keyword") String keyword,
+            Model model) {
         if(sortDir == null || sortDir.isEmpty()) {
             sortDir = "asc";
         }
         CategoryPageInfo pageInfo = new CategoryPageInfo();
-        List<Category> listCategories = service.listByPage(pageInfo, pageNum, sortDir);
+        List<Category> listCategories = service.listByPage(pageInfo, pageNum, sortDir, keyword);
+
+        long startCount = (pageNum - 1) * CategoryService.ROOT_CATEGORIES_PER_PAGE + 1;
+        long endCount = startCount + CategoryService.ROOT_CATEGORIES_PER_PAGE - 1;
+        if(endCount > pageInfo.getTotalElements()) {
+            endCount = pageInfo.getTotalElements();
+        }
 
         String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
 
         model.addAttribute("totalPages", pageInfo.getTotalPages());
         model.addAttribute("totalItems", pageInfo.getTotalElements());
+        model.addAttribute("startCount", startCount);
+        model.addAttribute("endCount", endCount);
         model.addAttribute("currentPage", pageNum);
         model.addAttribute("sortField", "name");
         model.addAttribute("sortDir", sortDir);
+        model.addAttribute("keyword", keyword);
         model.addAttribute("listCategories", listCategories);
         model.addAttribute("reverseSortDir", reverseSortDir);
 
