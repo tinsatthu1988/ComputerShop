329c74989dfab100a00ac0cdeea2ef80c3f9db35
tin <tinsatthu1988@gmail.com>
2021-07-18 22:39:30 +0700

    017 Exercise_ Code Export Categories to CSV

--- Shopme/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryController.java
+++ Shopme/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class CategoryController {
     @Autowired
@@ -136,5 +138,13 @@ public class CategoryController {
             redirectAttributes.addFlashAttribute("message", e.getMessage());
         }
         return "redirect:/categories";
+    }
+
+    @GetMapping("/categories/export/csv")
+    public void exportToCSV(HttpServletResponse response) throws IOException {
+        List<Category> listCategories = service.listCategoriesUsedInForm();
+        CategoryCsvExporter exporter = new CategoryCsvExporter();
+        exporter.export(listCategories, response);
+
     }
 }
