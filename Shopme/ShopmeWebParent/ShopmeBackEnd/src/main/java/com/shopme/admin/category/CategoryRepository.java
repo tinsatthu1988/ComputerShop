9d276adf87d4e026f88164f2e681f00441832a14
tin <tinsatthu1988@gmail.com>
2021-07-18 21:30:03 +0700

    014 Code Pagination for Categories List

--- Shopme/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryRepository.java
+++ Shopme/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryRepository.java
@@ -1,5 +1,7 @@
 package com.shopme.admin.category;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -12,6 +14,9 @@ import java.util.List;
 public interface CategoryRepository extends PagingAndSortingRepository<Category, Integer> {
     @Query("SELECT c FROM Category c WHERE c.parent.id is NULL")
     public List<Category> findRootCategories(Sort sort);
+
+    @Query("SELECT c FROM Category c WHERE c.parent.id is NULL")
+    public Page<Category> findRootCategories(Pageable pageable);
 
     public long countById(Integer id);
 
