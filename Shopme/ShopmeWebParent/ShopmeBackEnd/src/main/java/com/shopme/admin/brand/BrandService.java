68bb4517026a35694877b7aaab29c1e13211ff52
tin <tinsatthu1988@gmail.com>
2021-07-19 17:58:07 +0700

    011 Exercise_ Code Pagination, Sort and Filter for Brands Listing

--- Shopme/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandService.java
+++ Shopme/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandService.java
@@ -6,6 +6,10 @@ import java.util.NoSuchElementException;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.shopme.common.entity.Brand;
@@ -14,11 +18,26 @@ import com.shopme.common.entity.Brand;
 @Transactional
 public class BrandService {
 
+    public static final int BRANDS_PER_PAGE = 4;
     @Autowired
     private BrandRepository repo;
 
     public List<Brand> listAll(){
         return (List<Brand>) repo.findAll();
+    }
+
+    public Page<Brand> listByPage(int pageNum, String sortField, String sortDir, String keyword){
+        Sort sort = Sort.by(sortField);
+
+        sort = sortDir.equals("asc") ? sort.ascending() : sort.descending();
+
+        Pageable pageable = PageRequest.of(pageNum - 1, BRANDS_PER_PAGE, sort);
+
+        if(keyword != null) {
+            return repo.findAll(keyword, pageable);
+        }
+
+        return repo.findAll(pageable);
     }
 
     public Brand save(Brand brand) {
