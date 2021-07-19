31934592eae3a56c38cafb07b866ce1ca150c680
tin <tinsatthu1988@gmail.com>
2021-07-19 13:57:39 +0700

    006 Exercise_ Code Brand Form

--- Shopme/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandController.java
+++ Shopme/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandController.java
@@ -2,6 +2,8 @@ package com.shopme.admin.brand;
 
 import java.util.List;
 
+import com.shopme.admin.category.CategoryService;
+import com.shopme.common.entity.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,13 +15,27 @@ import com.shopme.common.entity.Brand;
 public class BrandController {
 
     @Autowired
-    private BrandService service;
+    private BrandService brandService;
+
+    @Autowired
+    private CategoryService categoryService;
 
     @GetMapping("/brands")
     public String listAll(Model model) {
-        List<Brand> listBrands = service.listAll();
+        List<Brand> listBrands = brandService.listAll();
 
         model.addAttribute("listBrands", listBrands);
         return "brands/brands";
+    }
+
+    @GetMapping("/brands/new")
+    public String newBrand(Model model) {
+        List<Category> listCategories = categoryService.listCategoriesUsedInForm();
+
+        model.addAttribute("listCategories", listCategories);
+        model.addAttribute("brand", new Brand());
+        model.addAttribute("pageTitle", "Create New Brand");
+
+        return "brands/brand_form";
     }
 }
