0b5f282bccb900f73e87940868977d555a1fe47e
tin <tinsatthu1988@gmail.com>
2021-07-20 17:51:59 +0700

    006 Code Product Overview Form

--- Shopme/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
+++ Shopme/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
@@ -2,18 +2,24 @@ package com.shopme.admin.product;
 
 import java.util.List;
 
+import com.shopme.admin.brand.BrandService;
+import com.shopme.common.entity.Brand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.shopme.common.entity.Product;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProductController {
 
     @Autowired
     private ProductService productService;
+
+    @Autowired
+    private BrandService brandService;
 
     @GetMapping("/products")
     public String listAll(Model model) {
@@ -22,5 +28,29 @@ public class ProductController {
         model.addAttribute("listProducts", listProducts);
 
         return "products/products";
+    }
+
+    @GetMapping("/products/new")
+    public String newProduct(Model model) {
+        List<Brand> listBrands = brandService.listAll();
+
+        Product product = new Product();
+        product.setEnabled(true);
+        product.setInStock(true);
+
+        model.addAttribute("product", product);
+        model.addAttribute("listBrands", listBrands);
+        model.addAttribute("pageTitle", "Create New Product");
+
+        return "products/product_form";
+    }
+
+    @PostMapping("/products/save")
+    public String saveCategory(Product product)  {
+        System.out.println("Product name: " + product.getName());
+        System.out.println("Brand ID: " + product.getBrand().getId());
+        System.out.println("Category ID: " + product.getCategory().getId());
+
+        return "redirect:/products";
     }
 }
