d4343c1068be9d7526ae3eecfe798c090a64b89b
tin <tinsatthu1988@gmail.com>
2021-07-22 20:26:31 +0700

    023 Code Pagination, Sort and Filter for Products Page

--- Shopme/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
+++ Shopme/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
@@ -15,6 +15,8 @@ import com.shopme.common.entity.ProductImage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -40,10 +42,39 @@ public class ProductController {
     private BrandService brandService;
 
     @GetMapping("/products")
-    public String listAll(Model model) {
-        List<Product> listProducts = productService.listAll();
+    public String listFirstPage(Model model) {
+        return listByPage(1, model, "name", "asc", null);
+    }
 
+    @GetMapping("/products/page/{pageNum}")
+    public String listByPage(
+            @PathVariable(name="pageNum") int pageNum, Model model,
+            @Param("sortField") String sortField, @Param("sortDir") String sortDir,
+            @Param("keyword") String keyword
+    ) {
+
+        Page<Product> page = productService.listByPage(pageNum, sortField, sortDir, keyword);
+        List<Product> listProducts = page.getContent();
+
+        long startCount = (pageNum - 1) * ProductService.PRODUCTS_PER_PAGE + 1;
+        long endCount = startCount + ProductService.PRODUCTS_PER_PAGE - 1;
+        if(endCount > page.getTotalElements()) {
+            endCount = page.getTotalElements();
+        }
+
+        String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+
+        model.addAttribute("currentPage", pageNum);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("startCount", startCount);
+        model.addAttribute("endCount", endCount);
+        model.addAttribute("totalItems", page.getTotalElements());
         model.addAttribute("listProducts", listProducts);
+
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", reverseSortDir);
+        model.addAttribute("keyword", keyword);
 
         return "products/products";
     }
@@ -196,7 +227,7 @@ public class ProductController {
     }
 
     @GetMapping("/products/delete/{id}")
-    public String deleteCategory(@PathVariable(name = "id") Integer id, RedirectAttributes redirectAttributes) throws ProductNotFoundException {
+    public String deleteProduct(@PathVariable(name = "id") Integer id, RedirectAttributes redirectAttributes) throws ProductNotFoundException {
         try {
             productService.delete(id);
 
--- Shopme/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductRepository.java
+++ Shopme/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.shopme.admin.product;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -14,4 +16,11 @@ public interface ProductRepository extends PagingAndSortingRepository<Product, I
     @Query("UPDATE Product p SET p.enabled = ?2 WHERE p.id = ?1")
     @Modifying
     public void updateEnabledStatus(Integer id, boolean enabled);
+
+    @Query("SELECT p FROM Product p WHERE p.name LIKE %?1% "
+            + "OR p.shortDescription LIKE %?1% "
+            + "OR p.fullDescription LIKE %?1% "
+            + "OR p.brand.name LIKE %?1% "
+            + "OR p.category.name LIKE %?1% ")
+    public Page<Product> findAll(String keyword, Pageable pageable);
 }
