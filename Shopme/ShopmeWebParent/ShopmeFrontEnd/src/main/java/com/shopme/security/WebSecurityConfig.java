a62695da8a037a5242a6f3a4b13a2f42a8bb4b73
tin <tinsatthu1988@gmail.com>
2021-08-07 19:34:33 +0700

    002 Exercise_ Code Customer Login and Logout

--- Shopme/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/security/WebSecurityConfig.java
+++ Shopme/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/security/WebSecurityConfig.java
@@ -23,8 +23,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
-
+        http.authorizeRequests()
+            .antMatchers("/customer").authenticated()
+            .anyRequest().permitAll()
+            .and()
+            .formLogin()
+                .loginPage("/login")
+                .usernameParameter("email")
+                .permitAll()
+            .and()
+            .logout().permitAll()
+            .and()
+            .rememberMe()
+                .key("1234567890_aBcDefGhiJ")
+                .tokenValiditySeconds(14 * 24 * 60 * 60);
     }
 
     @Override
@@ -32,4 +44,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**");
     }
 
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new CustomerUserDetailsService();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
 }
